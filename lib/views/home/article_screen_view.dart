704d75d6ce0f5b378d838ab09c0ed0145fd9f1c4
David Tawfik <david.tawfik.intern@linkdevelopment.com>
2024-07-17 12:00:40 +0300

    Added appBar in article creen

--- lib/views/home/article_screen_view.dart
+++ lib/views/home/article_screen_view.dart
@@ -3,45 +3,70 @@ import 'package:intl/intl.dart';
 import 'package:link_dev_app/view_models/article_view_model.dart';
 import 'package:link_dev_app/views/home/article_view.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 class ArticleScreenView extends StatelessWidget {
   final ArticleViewModel articleViewModel;
   Future<void> _launchUrl() async {
-  final Uri _url = Uri.parse(articleViewModel.url);
-  if (!await launchUrl(_url)) {
-    throw Exception('Could not launch $_url');
+    final Uri url = Uri.parse(articleViewModel.url);
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
   }
-}
 
   ArticleScreenView({super.key, required this.articleViewModel});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        
-        children: [
-          ArticleView(
-          isScreen: true,
-          title: articleViewModel.title,
-          author: articleViewModel.author,
-          description: articleViewModel.description,
-          date: DateFormat.yMMMd()
-                                  .format(articleViewModel.publishedAt),
-          imageUrl: articleViewModel.urlToImage,
-          ), 
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color?>(Colors.black),
-              foregroundColor: MaterialStateProperty.all<Color?>(Colors.white),
-              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              ),
-            ),
-            onPressed: _launchUrl,
-            
-            child: const Text('OPEN WEBSITE'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const ImageIcon(AssetImage('assets/images/search.png')),
+            onPressed: () {},
           ),
         ],
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.grey[900],
+        title: Text(
+          'LINK DEVELOPMENT',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ArticleView(
+              isScreen: true,
+              title: articleViewModel.title,
+              author: articleViewModel.author,
+              description: articleViewModel.description,
+              date: DateFormat.yMMMd().format(articleViewModel.publishedAt),
+              imageUrl: articleViewModel.urlToImage,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color?>(Colors.black),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color?>(Colors.white),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                ),
+                onPressed: _launchUrl,
+                child: const Text('OPEN WEBSITE'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
